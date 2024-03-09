3e9c5f48fad967ff887386feba9d4c35bd090aa1
jforellana <mrjfob@byu.edu>
2024-03-08 18:36:11 -0700

    new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import basketball from "./CollegeBasketballTeams.json";
+
+function HeadSection() {
+  return (
+    <div className="Header">
+      <h1>Welcome to the basketball site!</h1>
+      <p>This is site contains information about NCAA's March Madness</p>
+    </div>
+  );
+}
+
+function BData() {
+  return (
+    <div className="data1">
+      <div className="data1Title">INFORMATION FOR EACH TEAM</div>
+      <div className="data">
+        {basketball &&
+          basketball.teams.map((r) => {
+            return (
+              <div className="Card">
+                <div>
+                  <div className="SName">School Name</div>
+                  {r.school}
+                </div>
+                <div>
+                  <div className="SName">Mascot Name</div>
+                  {r.name}
+                </div>
+                <div>
+                  <div className="SName">Location</div>
+                  {r.city}, {r.state}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+function Teams() {
+  return (
+    <div className="teams1">
+      <div className="data1Title">LIST OF ALL TEAMS</div>
+      <div className="teamList">
+        {basketball &&
+          basketball.teams.map((r) => {
+            return <div>{r.name}</div>;
+          })}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <HeadSection />
+      <div className="Flex">
+        <BData />
+        <Teams />
+      </div>
     </div>
   );
 }
